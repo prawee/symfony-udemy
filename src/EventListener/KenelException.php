bd9df8b9230bd6cb2e513261c0ff28ec82545709
Prawee Wongsa <prawee@hotmail.com>
2020-04-23 02:23:06 +0700

    how to create event listener and register to service

---
+++ src/EventListener/KenelException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link http://www.thaiyii.com
+ * 2020-04-23 02:06
+ * @copyright Copyright (c) 2020 served
+ * @author Prawee Wongsa <prawee@hotmail.com>
+ * @license MIT
+ */
+
+namespace App\EventListener;
+
+
+class KenelException
+{
+    public function onKernelException()
+    {
+        die('I am listener');
+    }
+}

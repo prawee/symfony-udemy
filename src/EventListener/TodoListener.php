57c8801498fac5f7aec7a55ae01f3e020e4ec272
Prawee Wongsa <prawee@hotmail.com>
2020-04-23 03:36:50 +0700

    how to using event listener with doctrine

---
+++ src/EventListener/TodoListener.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link http://www.thaiyii.com
+ * 2020-04-23 03:26
+ * @copyright Copyright (c) 2020 served
+ * @author Prawee Wongsa <prawee@hotmail.com>
+ * @license MIT
+ */
+
+namespace App\EventListener;
+
+
+use App\Entity\Todo;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+
+class TodoListener
+{
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        //dump('Hello from postPersist event listener TODO');
+        $entity = $args->getObject();
+        if (!$entity instanceof Todo)
+            return;
+
+        echo $entity->getName();
+    }
+}
